5cdc7985736a9de62f32f84381b2b2dcf74a9b8c
Inés Villalba <paprika.sputnik@gmail.com>
2018-08-13 18:25:25 +0200

    Afinado el componente Novedades; estructura, datos y tags

--- src/app/novedades/novedades.component.ts
+++ src/app/novedades/novedades.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RestaurantsService } from '../restaurants.service';
 
 @Component({
   selector: 'app-novedades',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NovedadesComponent implements OnInit {
 
-  constructor() { }
+  restaurants: any[];
+  tag: boolean; 
+
+  constructor(private restaurantsService: RestaurantsService) { }
 
   ngOnInit() {
+    this.restaurantsService.getRestaurants().then((response)=>{
+      console.log(response.json());
+      this.restaurants = response.json();
+    })
   }
 
+  twoFoodTags(restaurant){
+    return restaurant.slice(0,2);
+  }
 }
