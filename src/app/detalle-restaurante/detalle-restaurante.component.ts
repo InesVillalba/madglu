2d7eff5eec3d68793afac8faf1e28b87e52d6571
Inés Villalba <paprika.sputnik@gmail.com>
2018-08-21 16:40:39 +0200

    Creado componente DetalleRestaurante

---
+++ src/app/detalle-restaurante/detalle-restaurante.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-detalle-restaurante',
+  templateUrl: './detalle-restaurante.component.html',
+  styleUrls: ['./detalle-restaurante.component.css']
+})
+export class DetalleRestauranteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
