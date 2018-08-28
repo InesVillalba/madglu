fe91bf6789b4c7d42d723f0cce11166cf2bc5ba9
Inés Villalba <paprika.sputnik@gmail.com>
2018-08-28 21:24:21 +0200

    Creado componente card-restaurant

---
+++ src/app/card-restaurant/card-restaurant.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-card-restaurant',
+  templateUrl: './card-restaurant.component.html',
+  styleUrls: ['./card-restaurant.component.css']
+})
+export class CardRestaurantComponent implements OnInit {
+
+  @Input() cardRestaurant = [];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
